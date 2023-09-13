c44bc7abdd0668aceaa84c975ecb37fb9fae13d6
mageeus <mateusk747@gmail.com>
2023-09-13 17:27:15 -0300

    first commit

---
+++ form_perfil.php
@@ -0,0 +1,12 @@
+<?php
+require_once("class/class.Login.php");
+require_once("class/class.Aluno.php");
+require_once("header.php");
+$obj_login->revalidarLogin();
+?>
+
+<body>
+
+    <?php require_once("menu.php") ?>
+
+</body>
