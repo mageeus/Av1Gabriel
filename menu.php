cafb85d07be9781a5b14aecb08a61d931b998c2f
mageeus <mateusk747@gmail.com>
2023-09-15 20:55:52 -0300

    aba criar perfil no menu.php

--- menu.php
+++ menu.php
@@ -1,5 +1,6 @@
 <div class="sidenav">
         <a href="welcome.php">Página Inicial</a>
         <a href="form_pessoa.php">Perfil</a>
+        <a href="#">Criar perfil</a>
         <a href="proc.sessiondestroy.php">Sair</a>
 </div>
