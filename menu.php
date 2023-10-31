d2bd367f85f254f810fc852c0e3892bca129fba5
mageeus <mateusk747@gmail.com>
2023-10-30 21:18:46 -0300

    removi criar conta

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <div class="sidenav">
-        <a href="welcome.php">Página Inicial</a>
+        <a href="form_publicacao.php">Página Inicial</a>
         <a href="form_pessoa.php">Perfil</a>
-        <a href="form_criarPessoa.php">Criar perfil</a>
+        <!--        <a href="form_criarPessoa.php">Criar perfil</a>     -->
         <a href="proc.sessiondestroy.php">Sair</a>
 </div>
