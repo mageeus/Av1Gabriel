611c84989066e3654945a1e031311de935732432
mageeus <mateusk747@gmail.com>
2023-11-01 13:19:41 -0300

    editar e apagar publicacao feito (somente a arte)

--- form_publicacao.php
+++ form_publicacao.php
@@ -13,11 +13,6 @@ $obj_login->revalidarLogin();
     <div class="content">
         <h2>Sua Publicacao</h2>
         <div>
-            <form action="form_publicacao.php" method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="idPessoa" value="<?php echo $_SESSION['idPessoa'] ?>" />
-                <input type="file" id="Imagem" name="Imagem" accept="image/png, image/png" required />
-                <input type="submit" value="Criar Publicação" name="comando">
-            </form>
             <?php
             if (isset($_POST['comando']) && $_POST['comando'] == 'Criar Publicação') {
                 $imagem = $_FILES['Imagem'];
@@ -30,11 +25,51 @@ $obj_login->revalidarLogin();
                 $type = $imagem['type'];
 
                 $blob = addslashes((file_get_contents($imagem["tmp_name"])));
+
                 $idArte = ($Arte->InsertArte($_POST['idPessoa'], $blob)[0]['idArte']);
                 $Publicacao->CriarPublicacao($_SESSION['idPessoa'], $idArte);
+
                 header("location:form_publicacao.php?comando=alteracaook");
-            } elseif ((isset($_POST['comando']) && $_POST['comando'] == 'Selecionar') /*&& $_SESSION['idPessoa'] == $pub['idPessoa']*/) {
-                var_dump($_GET);
+            } elseif (isset($_GET['idPublicacao'])) {
+            ?>
+                <form action="form_publicacao.php" method="POST" enctype="multipart/form-data">
+                    <input type="file" id="Arte" name="Arte" accept="image/png, image/png" required />
+                    <input type="submit" value="Editar Publicação" name="comando">
+                    <td> <a href=form_publicacao.php> <input type="button" value="Cancelar"> </td>
+                </form>';
+                <form action="form_publicacao.php" method="POST" enctype="multipart/form-data">
+                    <input type="submit" value="Apagar Publicação" name="comando">
+                </form>';
+
+            <?php
+                $_SESSION['idArte'] = $_GET['idArte'];
+                $_SESSION['idPublicacao'] = $_GET['idPublicacao'];
+            } elseif (isset($_POST['comando']) && $_POST['comando'] == 'Editar Publicação') {
+                $imagem = $_FILES['Arte'];
+                $info = getimagesize($imagem["tmp_name"]);
+
+                if (!$info) {
+                    die("arquivo não é uma imagem");
+                }
+
+                $name = $imagem['name'];
+                $type = $imagem['type'];
+
+                $blob = addslashes((file_get_contents($imagem["tmp_name"])));
+
+                $Arte->EditArte($_SESSION['idArte'], $blob);
+
+                header('location:form_publicacao.php');
+            } elseif (isset($_POST['comando']) && $_POST['comando'] == 'Apagar Publicação') {
+                $Publicacao->deletePublicacao($_SESSION['idPublicacao']);
+            } else {
+            ?>
+                <form action="form_publicacao.php" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="idPessoa" value="<?php echo $_SESSION['idPessoa'] ?>" />
+                    <input type="file" id="Imagem" name="Imagem" accept="image/png, image/png" required />
+                    <input type="submit" value="Criar Publicação" name="comando" />
+                </form>
+            <?php
             }
             ?>
             <table>
@@ -61,17 +96,24 @@ $obj_login->revalidarLogin();
 
                 //var_dump($_SESSION);
 
+                /*
+                coisas salvas no SESSION
+                    Username;
+                    Senha;
+                    token;
+                    idPessoa; 
+                */
+
                 foreach ($publicacao as $pub) {
                     echo "<tr>";
-                    //echo '<input type="hidden" value=' . $pub['idPessoa'] . '/>';
+                    echo '<input type="hidden" value=' . $pub['idPessoa'] . '/>';
                     echo "<td>"  . $pub['idPublicacao'] . "</td>";
                     echo "<td>" . $pub['username'] . "</td>";
                     echo "<td>" . "<img class='Arte' src='data:image/*;base64," . base64_encode($pub["arte"]) . "' />" . "</td>";
-                    echo "<td> <a href=form_publicacao.php?selecionaPub=" . $pub['idPublicacao'] . ">" . '<input type="button" value="Selecionar" name="comando">' . "</td>";
-                    /*echo "<td>" . '' . "</td>";
-                    echo "<td>" . '' . "</td>"; */
+                    if ($pub['idPessoa'] == $_SESSION['idPessoa']) {
+                        echo "<td> <a href=form_publicacao.php?idPublicacao=" . $pub['idPublicacao'] . "&idPessoa=" . $pub['idPessoa'] . '&idArte=' . $pub['idArte'] . ">" . '<input type="button" value="Selecionar" name="comando">' . "</td>";
+                    }
                     echo "</tr>";
-                    //var_dump($publicacao);
                 }
                 ?>
             </table>
