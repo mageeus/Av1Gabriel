9dd03280dfdd20da0b416336d1b5b50a5c5a5846
mageeus <mateusk747@gmail.com>
2023-11-02 14:59:54 -0300

    mudei form_publicacao para form_feed

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <div class="sidenav">
-        <a href="form_publicacao.php">Página Inicial</a>
+        <a href="form_feed.php">Página Inicial</a>
         <a href="form_pessoa.php">Perfil</a>
         <!--        <a href="form_criarPessoa.php">Criar perfil</a>     -->
         <a href="proc.sessiondestroy.php">Sair</a>
